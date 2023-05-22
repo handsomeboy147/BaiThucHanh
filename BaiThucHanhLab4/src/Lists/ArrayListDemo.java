44402878ff2543bc372f02aefbe045617eba9479
handsomeboy147 <laminhdang17012003@gmail.com>
2023-05-22 20:45:51 +0700

    16/5

---
+++ BaiThucHanhLab4/src/Lists/ArrayListDemo.java
@@ -0,0 +1,7 @@
+package Lists;
+
+public class ArrayListDemo {
+   public static void main(String[] args){
+   ArrayList<String> list 
+   }
+}
---
+++ BaiThucHanhLab4/src/Student.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Student {
+    public static Scanner sc = new Scanner(System.in)
+    String Fullname;
+    int Age;
+
+    public static void Insert (List Student; int n ){
+        System.out.println("Nhap vao so phan tu: ");
+    }
