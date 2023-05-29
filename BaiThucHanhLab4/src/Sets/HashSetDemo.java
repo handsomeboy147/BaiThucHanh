03b025c289e487c7c8104cc2002d2abb421a7abc
handsomeboy147 <laminhdang17012003@gmail.com>
2023-05-29 19:55:24 +0700

    sets

--- BaiThucHanhLab4/src/Sets/HashSetDemo.java
+++ BaiThucHanhLab4/src/Sets/HashSetDemo.java
@@ -1,5 +1,47 @@
 package Sets;
 
-public class HashSetDemo {
+import java.util.HashSet;
+import java.util.Scanner;
+import Student.Student;
 
+public class HashSetDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Student> std = new HashSet<Student>();
+        Student st = new Student();
+
+        do {
+            System.out.println("===Quan ly sinh vien===");
+            System.out.println(
+                    "1. Tao n sinh vien\n2. Sua thong sinh vien\n3. Xoa mot sinh vien\n4. In thong tin toan bo sinh vien\n0. Thoat");
+            System.out.println("Vui long chon chuc nang");
+            String chon = sc.nextLine();
+            switch (chon) {
+                case "1":
+                    System.out.println("====Nhap thong tin====");
+                    System.out.print("Nhap so luong sinh vien: ");
+                    int n = Integer.parseInt(sc.nextLine());
+                    st.NhapThongTin(std, n, sc);
+                    break;
+                case "2":
+                    st.SuaThongTin(std, sc);
+                    break;
+                case "3":
+                    st.XoaSinhVien(std, sc);
+                    break;
+                case "4":
+                    st.HienThiThongTin(std);
+                    break;
+                default:
+                    System.out.println("Khong co chuc nang nay, vui long nhap lai!");
+                    System.out.println("");
+                    break;
+
+            }
+            if (chon.equals("0")) {
+                System.exit(0);
+            }
+        } while (true);
+
+    }
 }
--- BaiThucHanhLab4/src/slide16.java
+++ BaiThucHanhLab4/src/slide16.java
@@ -1,16 +1,16 @@
+
 import java.util.ArrayList;
 
 public class slide16 {
-public static void main(String[] args) {
-    ArrayList<Integer> arr = new ArrayList<>();
-    arr.add(1);
-    arr.add(1);
-    arr.add(5);
-    arr.add(1000);
-    System.out.println("cac phan tu co trong arr la: ");
-    for(int i = 0; i < arr.size(); i++)
-    {
-        System.out.println(arr.get(i));
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(1);
+        arr.add(5);
+        arr.add(1000);
+        System.out.println("cac phan tu co trong arr la: ");
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i));
+        }
     }
-}
 }
